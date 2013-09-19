4960ad6f0800199fd916e3de7e78b305d3e6e77c
hnkelau <henrik.laurentz@gmail.com>
2013-09-19 18:14:00 +0200

    Diverse omstruktureringar, la spelarens update() i player bl.a

--- tddd23blokz/src/com/tddd23/blokz/GameObject.java
+++ tddd23blokz/src/com/tddd23/blokz/GameObject.java
@@ -25,11 +25,11 @@ public class GameObject {
 
 	private boolean hasCollidedX, hasCollidedY;
 	protected boolean grounded;
-	private World world;
+	protected World world;
 	boolean facingLeft = true;
-	private Rectangle displacementBox;
-	private Block collidingBlock;
-	private Rectangle collidingRect;
+	protected Rectangle displacementBox;
+	protected Block collidingBlock;
+	protected Rectangle collidingRect;
 
 	public GameObject(Vector2 position, World world) {
 		this.position = position;
@@ -39,102 +39,11 @@ public class GameObject {
 	}
 
 	public void update() {
-		if(!(this instanceof Player))
-			return;
-		world.collisionRects.clear();
-//		playerPositionDebugText()
 		
-		
-		acceleration.y += world.getGravity().y;
-		if (state == State.IDLE) {
-			velocity.set(0,acceleration.y);
-		}
-		if(acceleration.y > Constants.MAX_FALLING_ACCELERATION)
-			acceleration.y = Constants.MAX_FALLING_ACCELERATION;
-
-		if (state == State.WALKING) {
-			if (facingLeft) {
-				acceleration.set(-SPEED, acceleration.y);
-				velocity.set(velocity.x + acceleration.x, acceleration.y);
-				if (velocity.x < -0.3)
-					velocity.set(-0.3f, velocity.y);
-
-			} else {
-				acceleration.set(SPEED, acceleration.y);
-				velocity.set(velocity.x + acceleration.x, acceleration.y);
-				if (velocity.x > 0.3)
-					velocity.set(0.3f, velocity.y);
-			}
-		}
-		boolean hasCollidedX = false;
-		boolean hasCollidedY = false;
-
-		// X axis collision handling
-		displacementBox = new Rectangle(world.getPlayer().position.x
-				+ world.getPlayer().velocity.x, world.getPlayer().position.y,
-				world.getPlayer().bounds.width, world.getPlayer().bounds.height);
-
-		collidingBlock = getCollidingBlock(displacementBox);
-		if (collidingBlock != null) {
-			world.collisionRects.add(collidingBlock.getPositionRectangle());
-			hasCollidedX = true;
-			collidingRect = collidingBlock.getPositionRectangle();
-			// state = State.IDLE;
-
-			if (velocity.x > 0) {
-				position.x = collidingBlock.position.x - bounds.width;
-
-			} else {
-				position.x = collidingBlock.position.x
-						+ collidingBlock.bounds.width;
-			}
-
-		}
-
-		// Y axis collision handling
-		displacementBox = new Rectangle(world.getPlayer().position.x,
-				world.getPlayer().position.y + world.getPlayer().velocity.y,
-				world.getPlayer().bounds.width, world.getPlayer().bounds.height);
-
-		collidingBlock = getCollidingBlock(displacementBox);
-
-		if (collidingBlock != null) {
-			world.collisionRects.add(collidingBlock.getPositionRectangle());
-			hasCollidedY = true;
-			collidingRect = collidingBlock.getPositionRectangle();
-		
-			if (velocity.y > 0){
-				grounded = true;
-			}else{ //Collision with block above player, 
-				acceleration.y =0;
-			}
-			
-			
-			
-			if (velocity.y < 0) {
-				position.y = collidingBlock.position.y + bounds.height;
-
-			} else {
-				position.y = collidingBlock.position.y
-						- collidingBlock.bounds.height;
-			}
-		}else{
-			grounded = false;
-		}
-
-		if (hasCollidedX)
-			velocity.x = 0;
-		if (hasCollidedY)
-			velocity.y = 0;
-
-		position.add(velocity);
 	}
 
-	private Block getCollidingBlock(Rectangle rect) {
+	protected Block getCollidingBlock(Rectangle rect) {
 		for (GameObject object : world.getDynamicObjects()) {
-			if (object instanceof Player) {
-				continue;
-			}
 			if (object.getPositionRectangle().overlaps(rect)) {
 				return (Block) object;
 			}
--- tddd23blokz/src/com/tddd23/blokz/Player.java
+++ tddd23blokz/src/com/tddd23/blokz/Player.java
@@ -1,6 +1,8 @@
 package com.tddd23.blokz;
 
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.tddd23.blokz.GameObject.State;
 
 public class Player extends GameObject {
 
@@ -12,6 +14,95 @@ public class Player extends GameObject {
 		if (grounded) {
 			acceleration.add(new Vector2(0, -0.5f));
 		}
+	}
+	
+	public void update(){
+//		playerPositionDebugText()
+		world.collisionRects.clear();
+		
+		acceleration.y += world.getGravity().y;
+		if (state == State.IDLE) {
+			velocity.set(0,acceleration.y);
+		}
+		if(acceleration.y > Constants.MAX_FALLING_ACCELERATION)
+			acceleration.y = Constants.MAX_FALLING_ACCELERATION;
+
+		if (state == State.WALKING) {
+			if (facingLeft) {
+				acceleration.set(-SPEED, acceleration.y);
+				velocity.set(velocity.x + acceleration.x, acceleration.y);
+				if (velocity.x < -0.3)
+					velocity.set(-0.3f, velocity.y);
+
+			} else {
+				acceleration.set(SPEED, acceleration.y);
+				velocity.set(velocity.x + acceleration.x, acceleration.y);
+				if (velocity.x > 0.3)
+					velocity.set(0.3f, velocity.y);
+			}
+		}
+		boolean hasCollidedX = false;
+		boolean hasCollidedY = false;
+
+		// X axis collision handling
+		displacementBox = new Rectangle(world.getPlayer().position.x
+				+ world.getPlayer().velocity.x, world.getPlayer().position.y,
+				world.getPlayer().bounds.width, world.getPlayer().bounds.height);
+
+		collidingBlock = getCollidingBlock(displacementBox);
+		if (collidingBlock != null) {
+			world.collisionRects.add(collidingBlock.getPositionRectangle());
+			hasCollidedX = true;
+			collidingRect = collidingBlock.getPositionRectangle();
+			// state = State.IDLE;
+
+			if (velocity.x > 0) {
+				position.x = collidingBlock.position.x - bounds.width;
+
+			} else {
+				position.x = collidingBlock.position.x
+						+ collidingBlock.bounds.width;
+			}
+
+		}
+
+		// Y axis collision handling
+		displacementBox = new Rectangle(world.getPlayer().position.x,
+				world.getPlayer().position.y + world.getPlayer().velocity.y,
+				world.getPlayer().bounds.width, world.getPlayer().bounds.height);
+
+		collidingBlock = getCollidingBlock(displacementBox);
+
+		if (collidingBlock != null) {
+			world.collisionRects.add(collidingBlock.getPositionRectangle());
+			hasCollidedY = true;
+			collidingRect = collidingBlock.getPositionRectangle();
+		
+			if (velocity.y > 0){
+				grounded = true;
+			}else{ //Collision with block above player, 
+				acceleration.y =0;
+			}
+			
+			
+			
+			if (velocity.y < 0) {
+				position.y = collidingBlock.position.y + bounds.height;
+
+			} else {
+				position.y = collidingBlock.position.y
+						- collidingBlock.bounds.height;
+			}
+		}else{
+			grounded = false;
+		}
+
+		if (hasCollidedX)
+			velocity.x = 0;
+		if (hasCollidedY)
+			velocity.y = 0;
+
+		position.add(velocity);
 	}
 
 }
