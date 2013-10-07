227201656cf4ee91043430356e2bd54ac7c6685e
robii <robinsilverhav@gmail.com>
2013-10-07 11:23:46 +0200

    removed old renderer

--- tddd23blokz-android/src/com/tddd23/blokz/MainActivity.java
+++ tddd23blokz-android/src/com/tddd23/blokz/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tddd23.blokz;
 
+import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
