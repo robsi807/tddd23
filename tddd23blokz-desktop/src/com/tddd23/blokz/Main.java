dd09cdcaaeeba5bdb947a464eaeb6d2af08a687d
hnkelau <henrik.laurentz@gmail.com>
2013-10-20 22:02:30 +0200

    Ändrade texten på fönstret

--- tddd23blokz-desktop/src/com/tddd23/blokz/Main.java
+++ tddd23blokz-desktop/src/com/tddd23/blokz/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "tddd23blokz";
+		cfg.title = "Wizards Tower";
 		cfg.useGL20 = true;
 		cfg.width = 1280 ;
 		cfg.height = 720;
